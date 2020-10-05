6f3798c7a10e04adaa64d924ae2d92334977970e
Vassilyev Denis <tarapiygin@gmail.com>
2020-10-05 13:10:30 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# ahj-sse-ws - backend
+--------------------
+- frontend: <a href="https://tarapiygin.github.io/ahj-sse-ws-frontend-2/">Github Pages</a>
+- backend: <a href="https://github.com/tarapiygin/ahj-sse-ws-backend-2/">Repo</a>
---
+++ server.js
@@ -0,0 +1,69 @@
+const http = require('http');
+const Koa = require('koa');
+const Router = require('koa-router');
+const { streamEvents } = require('http-event-stream');
+const koaBody = require('koa-body');
+const uuid = require('uuid');
+const WS = require('ws');
+const cors = require('@koa/cors');
+
+
+const app = new Koa();
+app.use(cors());
+app.use(koaBody({
+  text: true,
+  urlencoded: true,
+  multipart: true,
+  json: true,
+}));
+
+const router = new Router();
+const server = http.createServer(app.callback())
+const wsServer = new WS.Server({ server });
+
+const usersList = [];
+
+router.get('/users', async (ctx, next) => {
+  ctx.response.body = usersList;
+});
+
+router.post('/users', async (ctx, next) => {
+  usersList.push({...ctx.request.body, id: uuid.v4()});
+  ctx.response.status = 204
+});
+
+router.delete('/users/:name', async (ctx, next) => {
+  const index = usersList.findIndex(({ name }) => name === ctx.params.name);
+  if (index !== -1) {
+    usersList.splice(index, 1);
+  };
+  ctx.response.status = 204
+});
+
+wsServer.on('connection', (ws, req) => {
+  ws.on('message', msg => {
+    [...wsServer.clients]
+    .filter(o => {
+      return o.readyState === WS.OPEN;
+    })
+    .forEach(o => o.send(msg));
+  });
+  ws.on('close', msg => {
+    [...wsServer.clients]
+    .filter(o => {
+      return o.readyState === WS.OPEN;
+    })
+    .forEach(o => o.send(JSON.stringify({type: 'delete'})));
+    ws.close();
+  });
+  [...wsServer.clients]
+    .filter(o => {
+      return o.readyState === WS.OPEN;
+    })
+    .forEach(o => o.send(JSON.stringify({type: 'add'})));
+});
+
+
+app.use(router.routes()).use(router.allowedMethods());
+const port = process.env.PORT || 7070;
+server.listen(port);
